8fe5e8c9ebc22beaaa8c0e86468eaec8f2be3b92
Patrick Kranig <Patrick.Kranig@live.de>
2019-01-21 11:27:09 +0100

    Add files via upload

---
+++ CMD Befehl ausführen lassen/CMD Befehl ausführen lassen/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMD_Befehl_ausführen_lassen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process(); // Process Class - Ermöglicht den Zugriff auf lokale Prozesse und Remoteprozesse und das Starten und Anhalten lokaler Systemprozesse.
+            //erzeugt ein Object process mit der Methode Process
+            //man braucht halt ein Objekt(das sich die eingestellten Eigenschaften merkt), um die Befehle ausführen zu können :)
+            // process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden; //versteckt das cmd fenster und führt den Code im Hinterrgrund aus
+            process.StartInfo.FileName = "cmd.exe"; //starten der cmd anwendung (Arbeitet mit den Windows Umgebungsvariablen
+            // string cmd = "/C netstat -a"; // speichert das Kommando in eine Variable
+            process.StartInfo.Arguments = "/C netstat -a"; //man kann es direkt übergeben oder in einer string variable
+            process.Start(); //starte die cmd
+            Console.WriteLine();
+        }
+    }
+}
---
+++ DateienSpeichern/DateienSpeichern/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; //muss dazugeschrieben werden damit wir einen Namespace/Bibliotheken von IO benutzen können
+//IO steht für Input/Output
+
+namespace DateienSpeichern
+{
+    //https://www.youtube.com/watch?v=KjP9v7xPUQE
+    //https://www.lernmoment.de/csharp-programmieren/die-klasse-file/
+    //https://docs.microsoft.com/de-de/dotnet/csharp/programming-guide/file-system/how-to-write-to-a-text-file
+    //Alternative: https://www.youtube.com/watch?v=AT0-BSGEya4
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //Dateiname in variable speichern:
+            // string DateiName = @"Testdaten\Werktage.txt"; //Alternativ
+            /* Mit dem @ werden alle Zeichen die als “Escape-Sequenz” interpretiert werden, wie zum Beispiel das \, als normales
+             * Zeichen verwendet. Alternativ könntest du auch "C:\\temp\\test.txt" schreiben.
+             * */
+
+            //Array deklarieren
+            string[] Array = new string[20]; //neues array von der länge 20 generieren
+            Array[0] = "1. wert"; //lege den 1. wert des arrays fest
+            Array[1] = "2. wert"; //lege den 2. wert des arrays fest
+
+            //datei schreiben
+            File.WriteAllLines(@"test.txt", Array); //schreibt nur arrays!
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //irgendwas mir File.ReadAllLines
+                      
+            
+        }
+    }
+}
